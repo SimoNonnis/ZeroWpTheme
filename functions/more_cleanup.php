ac75a3eb10056c3c6186ab5159fcef31b3ca8bc9
Simon Nonnis <SimoNonnis@users.noreply.github.com>
2016-05-23 11:06:15 +0200

    Update more_cleanup.php

--- functions/more_cleanup.php
+++ functions/more_cleanup.php
@@ -7,21 +7,24 @@ if ( !function_exists('cleanup') ) :
 
 	function cleanup() {
 
-		// Removes <p> wrapper from images
+
 		function zero_filter_ptags_on_images($content){
     	return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 		}
 
-		// This removes inline width/height from images
-		function remove_thumbnail_dimensions( $html ) {
-    		$html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
-    		return $html;
-		}
+		function remove_image_attributes( $html ) {
+			$html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
+			return $html;
+    }
 
-		add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
-		add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10 );
-		// Removes attached image sizes as well
+
+		// Removes <p> wrapper from images
 		add_filter( 'the_content', 'zero_filter_ptags_on_images', 10 );
+		// This removes inline width/height from images
+		add_filter( 'post_thumbnail_html', 'remove_image_attributes', 10 );
+		add_filter( 'image_send_to_editor', 'remove_image_attributes', 10 );
+		// Removes attached image sizes as well
+		add_filter( 'the_content', 'remove_image_attributes', 10 );
 
 
 	}//cleanup
